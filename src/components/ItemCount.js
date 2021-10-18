c29e04503b24e8a2a629e88d562c3f641c8e03a7
Sebastian Guillier Asus <sebastianguillier@gmail.com>
2021-10-18 15:29:47 -0300

    V2-clase-6

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,53 +1,64 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
-const ItemCount = ({product}) => {
+const ItemCount = ({ product }) => {
 
     const [count, setCount] = useState(1)
+
+    const contratarHoras = () => {
+        count === 0 ?
+            alert(`Debes Seleccionar Una minima Cantidad De Horas`) :
+            alert(`Contratar ${count} Horas de ${product.service} ofrecidas por ${product.name}`)
+    }
+
+    const agrgarHorasCarrito = () => {
+        count === 0 ?
+            alert(`Debes Seleccionar Una minima Cantidad De Horas`) :
+            alert(`Agregar al Carrito ${count} Horas de ${product.service} ofrecidas por ${product.name}`)
+    }
+
+    const linkServicio = () => {
+        alert(`Aqui entrariamos a ver detalle del seervicio de ${product.service} ofrecidas por ${product.name}`)
+    }
 
 
     return (
         <div className="item-card">
 
             <div>
-                <p className="item-service">
+                <p 
+                    className="item-service"
+                    onClick={linkServicio}
+                >
                     {product.service}
                 </p>
                 <p className="item-name">
                     {product.name}
                 </p>
-                    
+
             </div>
             <div className="item-count">
-                <button 
+                <button
                     className="item-count-add"
-                    onClick={()=>{setCount(Math.max(count-1,0))}}
+                    onClick={() => { setCount(Math.max(count - 1, 0)) }}
                 >-</button>
                 <div>{count}</div>
-                <button 
-                    className="item-count-add" 
-                    onClick={()=>{setCount(Math.min(count+1, product.stock))}}
+                <button
+                    className="item-count-add"
+                    onClick={() => { setCount(Math.min(count + 1, product.stock)) }}
                 >+</button>
             </div>
-            <button 
+            <button
                 className="item-button"
-                onClick={()=>{
-                    count===0?
-                    alert(`Debes Seleccionar Una minima Cantidad De Horas`):
-                    alert(`Contratar ${count} Horas de ${product.service} ofrecidas por ${product.name}`)
-                }}
+                onClick={contratarHoras}
             >
                 Contratar Horas
             </button>
-            <button 
+            <button
                 className="item-button"
-                onClick={()=>{
-                    count===0?
-                    alert(`Debes Seleccionar Una minima Cantidad De Horas`):
-                    alert(`Agregar al Carrito ${count} Horas de ${product.service} ofrecidas por ${product.name}`)
-                }}
+                onClick={agrgarHorasCarrito}
             >
-                Agregar al Carrito 
+                Agregar al Carrito
             </button>
 
         </div>
